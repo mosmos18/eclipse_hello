b87d171e8cdb4f70fce82c0fb5317c7160cb25de
kiyeon <kiyeon@test.ac.kr>
2017-04-04 09:43:09 +0900

    add hohoho

--- Hello/src/HelloWorld.java
+++ Hello/src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello his World");
 		System.out.println("Hello your Wolrd");
 		System.out.println("Hello her World");
+		System.out.println("Hoyoyo");
 	}
 
 }
