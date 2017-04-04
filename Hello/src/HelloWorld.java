856a831a581b2bab8911bcc95e8166f6755fbdc6
kiyeon <kiyeon@test.ac.kr>
2017-04-04 09:44:10 +0900

    resolve conflict

--- Hello/src/HelloWorld.java
+++ Hello/src/HelloWorld.java
@@ -6,11 +6,8 @@ public class HelloWorld {
 		System.out.println("Hello his World");
 		System.out.println("Hello your Wolrd");
 		System.out.println("Hello her World");
-<<<<<<< HEAD
 		System.out.println("Hoyoyo");
-=======
 		System.out.println("Hellolo");
->>>>>>> refs/heads/hotfix
 	}
 
 }
