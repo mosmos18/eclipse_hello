754e2afd3ef8ba49443877566f706031e691ef0b
kiyeon <kiyeon@test.ac.kr>
2017-04-06 10:46:02 +0900

    fixed

--- Hello/src/PushPrint.java
+++ Hello/src/PushPrint.java
@@ -2,7 +2,7 @@
 public class PushPrint {
 
 	public static void main(String[] args) {
-		System.out.println("Add Java fiel");
+		System.out.println("Add Java file");
 		System.out.println("Hello in Local lol");
 	}
 
